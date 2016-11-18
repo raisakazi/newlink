5e1c7f98babbb1964ae18e7983ae5c14b4ffe342
raisakazi <raisa.gal@gmail.com>
2016-11-18 12:17:44 +0530

    added 2 files

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.2.1/material.indigo-pink.min.css">
+<link rel="stylesheet" href="first.css">
+<script defer src="https://code.getmdl.io/1.2.1/material.min.js"></script>
+	<title></title>
+</head>
+<body>
+<div  class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+<header class="mdl-layout__header">
+<div class="mdl-layout__header-row">
+<span class="mdl-layout-title">logo</span>
+<div class="mdl-layout-spacer"></div>
+<nav class="mdl-navigation mdl-layout--large-screen-only">
+<a class="mdl-navigation__link" href="">Home</a>
+<a class="mdl-navigation__link" href="">Setting</a>
+<a class="mdl-navigation__link" href="">About us</a>
+</nav>
+</div>
+</header>
+<div class="mdl-layout__drawer">
+<span class="mdl-layout-title">logo</span>
+<nav class="mdl-navigation">
+<a class="mdl-navigation__link" href="">inbox</a>
+<a class="mdl-navigation__link" href="">outbox</a>
+<a class="mdl-navigation__link" href="">sent</a>
+</nav></div>
+<div class="vid-container">
+<video id="Video1" class="bgvid back" autoplay="false" muted="muted" preload="auto" loop>
+    <source src="http://shortcodelic1.manuelmasiacsasi.netdna-cdn.com/themes/geode/wp-content/uploads/2014/04/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
+</video>
+<hgroup>
+  <h1>login</h1>
+ </hgroup>
+<form>
+  <div class="group">
+    <input type="text"><span class="highlight"></span><span class="bar"></span>
+    <label>Name</label>
+  </div>
+  <div class="group">
+    <input type="email"><span class="highlight"></span><span class="bar"></span>
+    <label>Email</label>
+  </div>
+  <button type="button" class="button buttonBlue">Submit
+    <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
+  </button>
+</form>
+
+<main class="mdl-layout__content "> </main>
+<footer class="mdl-mini-footer">
+	<div class ="mdl-mini-footer--left-section">
+	<!-- <div class="=mdl-logo">logo</div>-->
+	 <ul class="mdl-mini-footer--link-list">
+	 <li><a href="">LOGO</a></li>
+	 <li><a href="">HELP</a></li>
+	 <li><a href="">About</a></li>
+	 </ul>
+</footer>
+
+
+</main>
+</div>
+</body>
+</html>
